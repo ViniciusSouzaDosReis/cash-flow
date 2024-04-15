e9b361fa6beca1222e784f1bb9064cb84f2a818e
Vinicius Reis <vinireis010305@gmail.com>
2024-04-15 17:04:46 -0300

    create response error json class

---
+++ src/CashFlow.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,16 @@
+﻿namespace CashFlow.Communication.Responses;
+
+public class ResponseErrorJson
+{
+    public List<string> ErrorMessage { get; set; }
+
+    public ResponseErrorJson(string message)
+    {
+        ErrorMessage = [message];
+    }
+
+    public ResponseErrorJson(List<string >message)
+    {
+        ErrorMessage = message;
+    }
+}
