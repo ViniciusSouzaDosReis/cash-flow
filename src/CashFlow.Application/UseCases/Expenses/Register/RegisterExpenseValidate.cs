d925c870d6629324d3f347b42e2b940bfdd6f727
Vinicius Reis <vinireis010305@gmail.com>
2024-04-15 17:26:53 -0300

    refactor message error

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidate.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidate.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Communication.Requests;
+using CashFlow.Exception;
 using FluentValidation;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -9,18 +10,18 @@ public class RegisterExpenseValidate : AbstractValidator<RequestRegisterExpenseJ
     {
         RuleFor(expense => expense.Title)
             .NotEmpty()
-            .WithMessage("The title is required.");
+            .WithMessage(ResourceErrorMessages.TITLE_REQUIRED);
 
         RuleFor(expense => expense.Amount)
-            .GreaterThanOrEqualTo(0)
-            .WithMessage("The Amount must be greater than zero.");
+            .GreaterThan(0)
+            .WithMessage(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
 
         RuleFor(expense => expense.Date)
             .LessThanOrEqualTo(DateTime.UtcNow)
-            .WithMessage("Expenses cannot be for the futere.");
+            .WithMessage(ResourceErrorMessages.EXPENSES_CANNOT_BE_FOR_FUTURE);
 
         RuleFor(expense => expense.PaymentType)
             .IsInEnum()
-            .WithMessage("Payment Type is not valid.");
+            .WithMessage(ResourceErrorMessages.PAYMENT_TYPE_NOT_VALID);
     }
 }
