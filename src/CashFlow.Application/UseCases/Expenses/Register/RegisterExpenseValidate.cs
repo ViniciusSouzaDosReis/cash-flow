dea320a23666182eda0d9daf9689925cf6580d2d
Vinicius Reis <vinireis010305@gmail.com>
2024-04-15 15:16:43 -0300

    create register expense validate

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidate.cs
@@ -0,0 +1,26 @@
+﻿using CashFlow.Communication.Requests;
+using FluentValidation;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+
+public class RegisterExpenseValidate : AbstractValidator<RequestRegisterExpenseJson>
+{
+    public RegisterExpenseValidate()
+    {
+        RuleFor(expense => expense.Title)
+            .NotEmpty()
+            .WithMessage("The title is required.");
+
+        RuleFor(expense => expense.Amount)
+            .GreaterThan(0)
+            .WithMessage("The Amount must be greater than zero.");
+
+        RuleFor(expense => expense.Date)
+            .LessThanOrEqualTo(DateTime.UtcNow)
+            .WithMessage("Expenses cannot be for the futere.");
+
+        RuleFor(expense => expense.PaymentType)
+            .IsInEnum()
+            .WithMessage("Payment Type is not valid.");
+    }
+}
