04d8f96342a640a7abb5c1aff28590ab4a122ec8
Vinicius Reis <vinireis010305@gmail.com>
2024-04-22 18:55:34 -0300

    create Register Expense Validate test

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidateTests.cs
@@ -0,0 +1,28 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Tests.Expenses.Register;
+
+public class RegisterExpenseValidateTests
+{
+    [Fact]
+    public void Success()
+    {
+        //Arrange
+        var validate = new RegisterExpenseValidate();
+
+        var request = new RequestRegisterExpenseJson()
+        {
+            Amount = 100,
+            Date = DateTime.Now.AddDays(-1),
+            Description = "Description",
+            PaymentType = CashFlow.Communication.Enums.PaymentType.Cash,
+            Title = "Title",
+        };
+        //Act
+        var result = validate.Validate(request);
+
+        //Assert
+        Assert.True(result.IsValid);
+    }
+}
