e4ebe02adb2eb12b705d2190c4cdb1aa1b9d1e60
Vinicius Reis <vinireis010305@gmail.com>
2024-04-30 17:34:23 -0300

    Add dependency injection

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+
+internal class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=localhost;Database=cashflow;Uid=root;Pwd=root";
+
+        var version = new Version(8, 0, 33);
+        var serverVersion = new MySqlServerVersion(version);
+
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+}
