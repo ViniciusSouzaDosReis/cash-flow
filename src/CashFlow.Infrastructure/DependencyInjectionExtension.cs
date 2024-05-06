ca5b4cde6481d28956c8c87d1e378065992483e3
Vinicius Reis <vinireis010305@gmail.com>
2024-05-06 19:38:56 -0300

    add dbContext dependency

--- src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Infrastructure.DataAccess;
 using CashFlow.Infrastructure.DataAccess.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +8,17 @@ namespace CashFlow.Infrastructure;
 public static class DependencyInjectionExtension
 {
     public static void AddInfrastructure(this IServiceCollection services)
+    {
+        AddContext(services);
+        AddRepositories(services);
+    }
+
+    private static void AddContext(IServiceCollection services)
+    {
+        services.AddDbContext<CashFlowDbContext>();
+    }
+
+    private static void AddRepositories(IServiceCollection services)
     {
         services.AddScoped<IExpensesRepository, ExpensesRepository>();
     }
