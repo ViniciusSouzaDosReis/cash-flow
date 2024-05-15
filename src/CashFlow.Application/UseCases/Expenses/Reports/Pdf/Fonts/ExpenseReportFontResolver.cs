f406b5d8eff3142e4559d358060edf01cfbd4f1d
Vinicius Reis <vinireis010305@gmail.com>
2024-05-15 17:08:43 -0300

    feat: add font resolver

---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/ExpenseReportFontResolver.cs
@@ -0,0 +1,37 @@
+﻿using PdfSharp.Fonts;
+using System.Reflection;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+
+public class ExpenseReportFontResolver : IFontResolver
+{
+    public byte[]? GetFont(string faceName)
+    {
+        var stream = ReadFontFile(faceName);
+
+        if(stream is null)
+        {
+            stream = ReadFontFile(FontHelper.DEFAULT);
+        }
+
+        var length = (int)stream!.Length;
+
+        var data = new byte[length];
+
+        stream.Read(buffer: data, offset: 0, count:  length);
+
+        return data;
+    }
+
+    public FontResolverInfo? ResolveTypeface(string familyName, bool bold, bool italic)
+    {
+        return new FontResolverInfo(familyName);
+    }
+
+    public Stream? ReadFontFile(string fontName)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        return assembly.GetManifestResourceStream($"CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts.{fontName}.ttf");
+    }
+}
