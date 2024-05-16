a3a1e0b852f80a7b8ded633e8c3c7f3f5cb7bbfb
Vinicius Reis <vinireis010305@gmail.com>
2024-05-16 08:57:27 -0300

    create report route to pdf

--- src/CashFlow.Api/Controllers/ReportController.cs
+++ src/CashFlow.Api/Controllers/ReportController.cs
@@ -1,5 +1,8 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Reports.Excel;
+using CashFlow.Application.UseCases.Expenses.Reports.Pdf;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.ComponentModel;
 using System.Net.Mime;
 
 namespace CashFlow.Api.Controllers;
@@ -10,12 +13,29 @@ public class ReportController : ControllerBase
     [HttpGet("excel")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
-    public async Task<IActionResult> GetExcel([FromServices] IGenerateExpenseReportExcelUseCase useCase, [FromHeader] DateTime month)
+    public async Task<IActionResult> GetExcel(
+        [FromServices] IGenerateExpenseReportExcelUseCase useCase,
+        [FromHeader] DateTime month)
     {
         byte[] file = await useCase.Execute(month);
 
         if(file.Length > 0)
             return File(file, MediaTypeNames.Application.Octet, "report.xlsx");
+
+        return NoContent();
+    }
+    
+    [HttpGet("pdf")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetPdf(
+        [FromServices] IGenerateExpenseReportPdfUseCase useCase,
+        [FromHeader] DateTime month)
+    {
+        byte[] file = await useCase.Execute(month);
+
+        if(file.Length > 0)
+            return File(file, MediaTypeNames.Application.Pdf, "report.pdf");
 
         return NoContent();
     }
